8bf8b5cbe847899085d0a09da2d54c84e2091faa
JJatau <jjatau14@outlook.com>
2023-10-05 16:56:11 +0100

    script added

--- client/index.js
+++ client/index.js
@@ -1,2 +1,21 @@
-'use strict';
+// 'use strict';
+// const diary = document.querySelector('.diary-entry');
+// const toolbar = document.querySelector('.toolbar-container');
 
+// let prevScrollPos = diary.scrollY;
+
+// diary.addEventListener('scroll', () => {
+//   const currentScrollPos = diary.scrollY;
+
+//   if (prevScrollPos > currentScrollPos) {
+//     // User is scrolling up, show the toolbar
+//     toolbar.classList.add = 'show';
+//     toolbar.classList.remove = 'hide';
+//   } else {
+//     // User is scrolling down, hide the toolbar
+//     toolbar.classList.add = 'hide';
+//     toolbar.classList.remove = 'show';
+//   }
+
+//   prevScrollPos = currentScrollPos;
+// });
